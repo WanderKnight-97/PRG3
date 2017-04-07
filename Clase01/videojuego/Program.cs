b83af2462e9fcfcdb7b421edef20e1a2cf18af21
WanderKnight-97 <micaela.pereyra@hotmail.com>
2017-04-07 11:23:31 -0300

    actualizacion

--- Clase01/videojuego/Program.cs
+++ Clase01/videojuego/Program.cs
@@ -10,19 +10,14 @@ namespace Videojuego
     {
         static void Main(string[] args)
         {
-            bool end = false;
-            //bool anima = false;
-            bool vivo = true;
-            //bool sepuede = true;
-
-            string tecla = "";
+            string key = "";
             string p = "P";
-            //string pj2 = "p";
-            string limite = "█";
-
+            string limit = "█";
+            bool end = false;
+            bool alive = true;
             int min = 0;
             int max = 50;
-            int bCant = max / 4;
+            int bCant = 10;
             Random r = new Random();
             int x = max / 2;
             int y = max / 2;
@@ -30,17 +25,29 @@ namespace Videojuego
             int[] bY = new int[bCant];
             int randNum;
             int i = 0;
-            string[] bombas = new string[bCant];
-            while (i != bCant)
+            string[] bombs = new string[bCant];
+            string[] enemies = new string[4];
+            int[] eX = new int[enemies.Length];
+            int[] eY = new int[enemies.Length];
+            
+            for (i = 0; i < bCant; i++)
             {
                 randNum = r.Next(min + 1, max - 1);
                 bX[i] = randNum;
                 randNum = r.Next(min + 1, max - 1);
                 bY[i] = randNum;
-                bombas[i] = "B";
-                i++;
+                bombs[i] = "B";
             }
-             i = 0;
+            for (i = 0; i < enemies.Length; i++)
+            {
+                randNum = r.Next(min + 1, max - 1);
+                eX[i] = randNum;
+                randNum = r.Next(min + 1, max - 1);
+                eY[i] = randNum;
+                enemies[i] = "E";
+            }
+
+            i = 0;
             while (i != bCant)
             {
                 if (bX[i] == x && bY[i] == y)
@@ -50,65 +57,67 @@ namespace Videojuego
                 }
                 i++;
             }
-            Console.SetCursorPosition(0, y + 3);
-            Console.WriteLine("W A S D para mover al personaje(P). Presione x para salir");
-            while (end == false)
+            Console.SetCursorPosition(0, y + 2);
+            Console.WriteLine("W A S D para mover a P. Presione x para salir");
+            /*while (end == false)
             {
-
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo cki = Console.ReadKey();
+                }*/
                 i = 0;
                 while (i != bCant)
                 {
                     Console.SetCursorPosition(bX[i], bY[i]);
-                    Console.WriteLine(bombas[i]);
+                    Console.WriteLine(bombs[i]);
                     i++;
                 }
+
+                for (i = 0; i < enemies.Length; i++)
+                {
+                    randNum = r.Next(0, 8);
+                    if ((randNum / 2) == 0)
+                    {
+                        eX[i] += 1;
+                    }
+                    else if ((randNum / 2) != 0)
+                    {
+                        eX[i] -= 1;
+                    }
+                    Console.SetCursorPosition(eX[i], eY[i]);
+                    Console.WriteLine(enemies[i]);
+                }
+
                 Console.SetCursorPosition(x, y);
-                if (vivo == true)
+                if (alive == true)
                 {
 
                     if (x == max || y == max || x == min + 1 || y == min + 1)
                     {
-                        //sepuede = false;
-                        Console.WriteLine(limite);
+                        Console.WriteLine(limit);
                     }
                     else if (x != max || y != max || x != min + 1 || y != min + 1)
                     {
                         Console.WriteLine(p);
-                        //sepuede = true;
-
                     }
-                    /*if (sepuede == true)
-                    {
-                        if (anima == false)
-                        {
-                            Console.WriteLine(pj);
-                            anima = true;
-                        }
-                        else
-                        {
-                            Console.WriteLine(pj2);
-                            anima = false;
-                        }
-                    }*/
-
-                    tecla = Console.ReadKey().KeyChar.ToString();
-                    if (tecla == "w" && y > min + 1 || tecla == "W" && y > min + 1)
+                    key = Console.ReadKey().KeyChar.ToString();
+                    if (key == "w" && y > min + 1 || key == "W" && y > min + 1)
                     {
                         y -= 1;
                     }
-                    if (tecla == "s" && y <= max - 1 || tecla == "S" && y <= max - 1)
+                    if (key == "s" && y <= max - 1 || key == "S" && y <= max - 1)
                     {
                         y += 1;
                     }
-                    if (tecla == "a" && x > min + 1 || tecla == "A" && x > min + 1)
+                    if (key == "a" && x > min + 1 || key == "A" && x > min + 1)
                     {
                         x -= 1;
                     }
-                    if (tecla == "d" && x <= max - 1 || tecla == "D" && x <= max - 1)
+                    if (key == "d" && x <= max - 1 || key == "D" && x <= max - 1)
                     {
                         x += 1;
                     }
-                    if (tecla == "x" || tecla == "X")
+                    if (key == "x" || key == "X")
                     {
                         end = true;
                     }
@@ -117,26 +126,26 @@ namespace Videojuego
                     {
                         if (x == bX[i] && y == bY[i])
                         {
-                            vivo = false;
+                            alive = false;
                         }
                         i++;
                     }
 
                 }
                 Console.Clear();
-                if (vivo == false)
+                if (alive == false)
                 {
                     Console.SetCursorPosition(x, y);
                     Console.SetCursorPosition(x, y + 1);
-                    Console.WriteLine("GAME OVER");
-                    tecla = Console.ReadKey().KeyChar.ToString();
-                    if (tecla == "x" || tecla == "X")
+                    Console.WriteLine("Game over");
+                    key = Console.ReadKey().KeyChar.ToString();
+                    if (key == "x" || key == "X")
                     {
                         end = true;
                     }
                 }
                 Console.Clear();
-               
+
                 /*
                  a tener en cuenta en la creación de enemigos:
                  while (true)
@@ -147,17 +156,13 @@ namespace Videojuego
                     }
                     Console.Clear(); algo asi como dibujo
                     Syste.Threading.Thread.Sleep(500); setear para ajustar el "dormir" del while, asi los enemigos no son demasiado rapidos.
-
-                    Hacer clases para mejorar la prolijidad.
-
+                    
                     ignorar los bin y obj al comitear.
                 }
                  */
             }
-                }
-        
         }
-           
-        }
+
+    }
     
 
